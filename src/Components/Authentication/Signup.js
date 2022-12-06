b07dc6dbfbf8b163f29cc88f5907dd301e5a2f37
Omarfaruk4200 <emon35293@gmail.com>
2022-12-06 09:31:09 +0600

    added resolve

--- src/Components/Authentication/Signup.js
+++ src/Components/Authentication/Signup.js
@@ -25,9 +25,6 @@ const Signup = () => {
         const displayName = data.displayName
         await createUserWithEmailAndPassword(data?.email, data?.password)
         await updateProfile({ displayName })
-        // alert('Updated profile')
-        // console.log(data)
-
 
     }
 
@@ -122,62 +119,3 @@ const Signup = () => {
 }
 
 export default Signup
-
-// <div className='w-full'>
-// <img src="https://i.ibb.co/MS8q31b/sign-up-concept-illustration-114360-7875-removebg-preview.png" alt="Sign up page" />
-// </div>
-
-// <div className="card order-1 shadow-2xl bg-base-100">
-
-// <div className="flex w-full pt-3 mx-auto">
-//     <div className="grid h-15 flex-grow card  rounded-box place-items-center">
-//         <button onClick={() => handlegoogleSingin()} className="btn btn-primary">Google</button>
-//     </div>
-//     <div className="divider divider-horizontal">OR</div>
-//     <div className="grid h-15 flex-grow card  rounded-box place-items-center">
-//         <button className="btn ">GitHub</button>
-//     </div>
-// </div>
-// <hr className='mt-10' />
-// {/* from */}
-
-// <div className="card-body w-full">
-//     <form onSubmit={handleSubmit(onSubmit)}>
-//         <div className="form-control">
-//             <label className="label">
-//                 <span className="label-text">Name</span>
-//             </label>
-//             <input type="text" placeholder="name" className="input input-bordered" {...register("displayName", { required: true })} />
-//             {errors.name?.type === 'required' && "First name is required"}
-//         </div>
-
-//         <div className="form-control">
-//             <label className="label">
-//                 <span className="label-text">Email</span>
-//             </label>
-//             <input type="email" placeholder="email" className="input input-bordered" {...register("email", { required: "Email Address is required" })} />
-//             <p>{errors.email?.message}</p>
-//         </div>
-
-
-//         <div className="form-control">
-//             <label className="label">
-//                 <span className="label-text">Password</span>
-//             </label>
-//             <input type="password" placeholder="password" className="input input-bordered" {...register("password", { required: true })} />
-//             <p>{errors.password?.message}</p>
-
-//             <label className="label">
-
-
-//                 <Link to="/login">Login</Link>
-
-//             </label>
-//         </div>
-//         <div className="form-control mt-6">
-//             <button type="submit" className="btn btn-primary">Sign Up</button>
-//         </div>
-//     </form>
-
-// </div>
-// </div>
